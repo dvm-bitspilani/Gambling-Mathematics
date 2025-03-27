748aeb62f752752ceb3aa15c220ac8f27e4df471
Manas <manasck14@gmail.com>
2025-03-28 02:02:00 +0530

    fix: update game instructions to reflect changes in categories and rules

--- src/utils/useData.js
+++ src/utils/useData.js
@@ -28,21 +28,23 @@ const useInstructions = () => {
         "2. Teams will have 10 minutes to understand the rules and strategize.",
         "3. Teams will have a maximum of 50 minutes to attempt questions.",
         "4. Each team will begin with 3,000 points.",
-        "5. There are 16 total categories: 12 Math Categories (teams choose 11 to attempt) and 4 Puzzle Categories (mandatory for all teams).",
-        "6. Within each category, teams can choose:.",
+        "5. There are 15 total categories: 13 Math Categories and 2 Mental Aptitude.",
+        "6. Teams have to attempt exactly 12 categories from the given 15.",
+        "7. Within each category, teams can choose:",
         [
             "Easy Question (1.25x bet reward on correct answer).",
             "Medium Question (2x bet reward on correct answer).",
             "Hard Question (4x bet reward on correct answer)"
         ],
-        "7. Minimum bet amount: 200 points.",
-        "8. After betting you will be shown a question which will have 4 options out of which only 1 will be correct.",
-        "9. Incorrect or unanswered questions receive no points.",
-        "10. Teams will be penalized 10 percent of their final score for every math category they skip. Penalty is applied after all questions are attempted or time runs out.",
-        "11. Teams can attempt questions in any order.",
-        "12. All categories and difficulties are visible before making choices.",
-        "13. Minimum score cannot go below 0 points. If your coins are out the game will end.",
-        "14. Ties will be resolved on the basis of time taken to complete the game."
+        "8. Minimum bet amount: 200 points.",
+        "9. After betting you will be shown a question which will have 4 options out of which only 1 will be correct.",
+        "10. Incorrect or unanswered questions receive no points.",
+        "11. Teams will be penalized 10 percent of their final score for every math category they skip. Penalty is applied after all questions are attempted or time runs out.",
+        "12. Teams can attempt questions in any order.",
+        "13. All categories and difficulties are visible before making choices wisely.",
+        "14. Minimum score cannot go below 0 points. If your coins are out, the game will end.",
+        "15. Ties will be resolved on the basis of time taken to complete the game.",
+        "16. Users shall be logged out after 3 minutes of inactivity. Hence, submit your answer within 3 minutes."
     ];
 
     return instructions;
